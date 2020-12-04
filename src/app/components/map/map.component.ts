8dfd7e6ec9de22bb59eddc0aafadcf1d42082b47
Arthur REITER <arthur.reiter231@gmail.com>
2020-12-04 03:06:02 +0100

    add logo

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -12,6 +12,7 @@ export class MapComponent implements OnInit {
   constructor() {
   }
 
+
   ngOnInit() {
     this.myMap = L.map('mapId').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
